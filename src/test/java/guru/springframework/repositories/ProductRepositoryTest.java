82b324d29fb1d8fa9375b7519dda3107083d34d7
vale <crazy.vale91@hotmail.it>
2019-02-24 16:25:03 +0100

    test junit

--- src/test/java/guru/springframework/repositories/ProductRepositoryTest.java
+++ src/test/java/guru/springframework/repositories/ProductRepositoryTest.java
@@ -12,16 +12,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.math.BigDecimal;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//@SpringBootTest
 public class ProductRepositoryTest {
 
     private static final BigDecimal BIG_DECIMAL_100 = BigDecimal.valueOf(100.00);
     private static final String PRODUCT_DESCRIPTION = "a cool product";
     private static final String IMAGE_URL = "http://an-imageurl.com/image1.jpg";
 
-    @Autowired
-    private ProductRepository productRepository;
-
+    
     @Before
     public void setUp() throws Exception {
 
@@ -31,25 +28,4 @@ public class ProductRepositoryTest {
 public void testPersistence() {
 Assert.assertEquals(BIG_DECIMAL_100.compareTo(BigDecimal.valueOf(100.00)), 0);
 }
-
-    @Test
-@Ignore
-    public void testPersistence() {
-        //given
-        Product product = new Product();
-        product.setDescription(PRODUCT_DESCRIPTION);
-        product.setImageUrl(IMAGE_URL);
-        product.setPrice(BIG_DECIMAL_100);
-
-        //when
-        productRepository.save(product);
-
-        //then
-        Assert.assertNotNull(product.getId());
-        Product newProduct = productRepository.findById(product.getId()).orElse(null);
-        Assert.assertEquals((Long) 1L, newProduct.getId());
-        Assert.assertEquals(PRODUCT_DESCRIPTION, newProduct.getDescription());
-        Assert.assertEquals(BIG_DECIMAL_100.compareTo(newProduct.getPrice()), 0);
-        Assert.assertEquals(IMAGE_URL, newProduct.getImageUrl());
-    }
 }
